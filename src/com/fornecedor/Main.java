99158d99987719d599f6316aaf97ca2eb6a1d627
Manoel Martins <mano.martins29@gmail.com>
2021-11-18 00:03:56 -0300

    🎉 Initial commit

---
+++ src/com/fornecedor/Main.java
@@ -0,0 +1,9 @@
+package com.fornecedor;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Trabalho de engenharia");
+    }
+}
